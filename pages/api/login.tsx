e5ae7666f5184687a0d47f4c737f1e769ae06ebe
rootspyro <root.spyro@gmail.com>
2022-03-19 00:54:31 -0400

    login fetch and jwt added

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -1,19 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import sessionServices from "../../services/session-services";
+import jwt from "jsonwebtoken";
 
 export default async function Login(req : NextApiRequest, res : NextApiResponse) { 
 
 	if ( req.method == "POST" ) { 
 
-		const data = req.body;
+		const data = JSON.parse(req.body);
 		const user = await sessionServices.Login(data);
 
+
 		if ( user != null ) { 
-			res.json({user : "logged"}); 
+			res.json({ 
+				token : jwt.sign({ id : user.id }, process.env.SECRET),
+			})
 		}
 
 		else { 
-			res.json({user : "not logged"}); 
+			res.json(null);
 		}
 
 	}
--- pages/session/login.tsx
+++ pages/session/login.tsx
@@ -1,10 +1,32 @@
+import { useState } from "react"
 export default function Login(){
+
+	const [username, setUsername] = useState("")
+	const [password, setPassword] = useState("")
+
+	async function login(){
+
+		const requestBody = { 
+			user : username,
+			password : password
+		}
+
+		const data = await fetch("/api/login", {
+			method : "POST",
+			body : JSON.stringify(requestBody),
+		})
+
+		const json = await data.json()
+		console.log(json)
+
+	} 
+
 	return(
 		<>
-			<form method="POST" action="/api/login">
-				<input type="text" name="user" placeholder="username or email"></input>
-				<input type="password" name="password" placeholder="password"></input>
-				<button>Login</button>
+			<form>
+				<input type="text" name="user" placeholder="username or email" value={username} onChange={(e)=>setUsername(e.target.value)}></input>
+				<input type="password" name="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+				<button type="button" onClick={login}>Login</button>
 			</form>
 		</>
 	)
