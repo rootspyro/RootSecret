b3f4c10ad2a5db233e3de77e0e351f2f388353f0
rootspyro <root.spyro@gmail.com>
2022-04-19 14:39:37 -0400

    footer was not added to git stage

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() { 
+	return(
+		<>
+			<div className="bg-box fixed bottom-0 w-full flex justify-center p-2">
+				<p className="text-xs">Developed by <a className="text-theme underline" target="_blank" href="https://github.com/rootspyro">@rootspyro</a></p>
+			</div>
+		</>
+	)
+}
